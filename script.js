53a1a647d4715b751ac71665d55d9ad18f4418f7
Yan Lucas <119520321+Yanlucas10@users.noreply.github.com>
2023-05-11 08:56:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// variáveis 
+const elementoRespostas = document.querySelector('#message');
+const open = document.querySelector("#open");
+const again = document.querySelector("#again");
+const first1 = document.querySelector(".first1");
+const first2 = document.querySelector(".first2");
+let respostas = [
+  'A vida trará coisas boas se tiver paciência.',
+  'Não compense na ira o que lhe falta na razão.', 
+  'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+  'Não há que ser forte. Há que ser flexível.',
+  'A juventude não é uma época da vida, é um estado de espírito.'
+]
+
+let numeroAleatorio = Math.floor(Math.random() * respostas.length)
+
+elementoRespostas.innerHTML = respostas [numeroAleatorio]
+
+//eventos
+open.addEventListener('click', toggleFirst1);
+again.addEventListener('click', toggleFirst2);
+
+function toggleFirst1() {
+    toggleFirst2()
+    numeroAleatorio = Math.floor(Math.random() * respostas.length)
+}
+
+function toggleFirst2() {
+    toggleFirst()
+    
+}
+
+function toggleFirst() {
+    first1.classList.toggle("hide");
+    first2.classList.toggle("hide");
+    
+    
+}
