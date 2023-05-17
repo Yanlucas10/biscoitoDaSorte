aff3ad4f897246235fa2c0042ef0a65eaadcba49
Yan Lucas <119520321+Yanlucas10@users.noreply.github.com>
2023-05-17 11:07:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const open = document.querySelector("#open");
 const again = document.querySelector("#again");
 const first1 = document.querySelector(".first1");
 const first2 = document.querySelector(".first2");
-let respostas = [
+var respostas = [
   'A vida trará coisas boas se tiver paciência.',
   'Não compense na ira o que lhe falta na razão.', 
   'Defeitos e virtudes são apenas dois lados da mesma moeda.',
@@ -12,27 +12,29 @@ let respostas = [
   'A juventude não é uma época da vida, é um estado de espírito.'
 ]
 
-let numeroAleatorio = Math.floor(Math.random() * respostas.length)
-
-elementoRespostas.innerHTML = respostas [numeroAleatorio]
-
 //eventos
 open.addEventListener('click', toggleFirst1);
 again.addEventListener('click', toggleFirst2);
 
 function toggleFirst1() {
-    toggleFirst2()
     numeroAleatorio = Math.floor(Math.random() * respostas.length)
+    handleAnswer()
+    toggleFirst2()
 }
+
+function handleAnswer() {
+    elementoRespostas.innerHTML = respostas[numeroAleatorio]
+}
+
 
 function toggleFirst2() {
     toggleFirst()
-    
 }
+
 
 function toggleFirst() {
     first1.classList.toggle("hide");
     first2.classList.toggle("hide");
-    
-    
+
+
 }
